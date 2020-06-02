d113dc5eff1ab2f8bcd1e70cee37561e97c23525
deep.patel <deep.patel@nih.gov>
2020-06-02 09:15:58 -0400

    #141 grid result components

---
+++ src/metalnx-web/src/main/javascript/search/components/SearchStyleResultGrid.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+<b-table striped head-variant="dark" hover :items="getGridData.items" :fields="getGridData.fields">
+  <template slot="customTitle" slot-scope="data">
+    <b-link v-bind:href="data.item.url_link" target="_blank">{{ data.item.title }}</b-link>
+  </template>
+</b-table>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+    return {isActive: false}
+    },
+    name:'SearchStyleResultGrid',
+    props:['searchResult'],
+    computed: {
+      getGridData:  function () {
+        var resultData = [];
+        for (var entry in this.searchResult.search_result) {
+          resultData.push(this.searchResult.search_result[entry])
+        }
+        var data = {
+        fields: [
+          {
+            key: 'customTitle',
+            label: 'Title',
+            formatter: value => {
+              return value
+            }
+          },
+          {
+            key: 'subtitle',
+            label: 'Subtitle',
+            formatter: value => {
+              return value
+            }
+          },
+          {
+            key: 'content_text',
+            label: 'Content text',
+            formatter: value => {
+              return value
+            }
+          }
+        ],
+        items: resultData
+        }
+        return data;
+      }
+    }
+  }
+</script>
